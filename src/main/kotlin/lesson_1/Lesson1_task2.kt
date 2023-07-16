c0905abb964c2d5677e9305674b06f966dbf5a9e
abzhuravlev <jas1994@yandex.ru>
2023-07-16 19:05:16 +0300

    First task delivery

---
+++ src/main/kotlin/lesson_1/Lesson1_task2.kt
@@ -0,0 +1,7 @@
+package lesson_1
+    fun main() {
+        var amountOfOrders: Int = 75
+        var messageWithWish: String = "Воу, спасибо за покупку!"
+        println("${amountOfOrders}" + "\n"+
+            "${messageWithWish}")
+}
