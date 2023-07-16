605a359a47a35e7e9ebb8020c77581a6b4bcb062
abzhuravlev <jas1994@yandex.ru>
2023-07-16 20:08:55 +0300

    Fourth task delivery

---
+++ src/main/kotlin/lesson_1/Lesson4_task4.kt
@@ -0,0 +1,16 @@
+package lesson_1
+fun main() {
+    val length : Long = 40_868_600_000
+    val age : Byte = 27
+    val partOfDay : Double = 0.075
+    val seconds : Int = 6480
+    val partOfYear : Float = 2.0547945205479453E-4F
+    val apogee : Int = 327000
+
+    println("Расстояние - " + length + "\n" +
+    "Возраст - " + age + "\n" +
+    "Часть дня - " + partOfDay + "\n" +
+    "Секунды - "+ seconds + "\n" +
+    "Часть года - "+ partOfYear + "\n" +
+    "Апогей орбиты - " + apogee)
+}
